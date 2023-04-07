17478a8916e30ec14d1571433d33ac250ebe2b38
AlexsandroCS <Alexsandro.acs98@gmail.com>
2023-04-07 10:21:44 -0300

    Project created - User entity and resource

---
+++ course/src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -0,0 +1,17 @@
+package com.educandoweb.course.resources;
+
+import com.educandoweb.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "Alexsandro","Alexsandro.acs98@gmail.com","11000000000","a1b2c3d4");
+        return ResponseEntity.ok().body(u);
+    }
+}
