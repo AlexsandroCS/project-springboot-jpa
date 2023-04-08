24f433b38deb7295634ec7ae742e6171ff6af0a2
AlexsandroCS <Alexsandro.acs98@gmail.com>
2023-04-08 15:55:02 -0300

    Service layer, component registration

--- course/src/main/java/com/educandoweb/course/config/TestConfig.java
+++ course/src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -1,2 +1,26 @@
-package com.educandoweb.course.config;public class TestConfig {
+package com.educandoweb.course.config;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        User u1 = new User(null, "Alexsandro Grey", "Alexsandro@gmail.com", "111111111","123456");
+        User u2 = new User(null,"Fernanda Purple", "Fernanda@gmail.com","222222222","1234567");
+
+        userRepository.saveAll(Arrays.asList(u1, u2));
+    }
 }
--- course/src/main/java/com/educandoweb/course/repositories/UserRepository.java
+++ course/src/main/java/com/educandoweb/course/repositories/UserRepository.java
@@ -1,2 +1,8 @@
-package com.educandoweb.course.repositories;public class UserRepository {
+package com.educandoweb.course.repositories;
+
+import com.educandoweb.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
