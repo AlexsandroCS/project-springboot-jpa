a6b9a4ed246f92d9eb4105a921d196aa680abff8
AlexsandroCS <Alexsandro.acs98@gmail.com>
2023-04-12 04:08:47 -0300

    OrderStatus Enum

--- course/src/main/java/com/educandoweb/course/config/TestConfig.java
+++ course/src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.educandoweb.course.config;
 
 import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.entities.enums.OrderStatus;
 import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,9 +28,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Alexsandro Grey", "Alexsandro@gmail.com", "111111111","123456");
         User u2 = new User(null,"Fernanda Purple", "Fernanda@gmail.com","222222222","1234567");
 
-        Order o1 = new Order(null, Instant.parse("2023-04-12T02:46:02Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2023-04-12T02:46:22Z"), u1);
-        Order o3 = new Order(null, Instant.parse("2023-04-12T02:46:42Z"), u2);
+        Order o1 = new Order(null, Instant.parse("2023-04-12T02:46:02Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2023-04-12T02:46:22Z"), OrderStatus.WAITING_PAYMENTY, u1);
+        Order o3 = new Order(null, Instant.parse("2023-04-12T02:46:42Z"), OrderStatus.WAITING_PAYMENTY, u2);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
